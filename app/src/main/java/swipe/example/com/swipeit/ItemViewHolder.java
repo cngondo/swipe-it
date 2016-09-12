95a29f5a831b46e9d1f7a045a20a53c77b3f0288
cngondo <devngondo@gmail.com>
2016-09-12 23:58:09 +0300

    added necessary files for support

---
+++ app/src/main/java/swipe/example/com/swipeit/ItemViewHolder.java
@@ -0,0 +1,15 @@
+package swipe.example.com.swipeit;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by ngondo on 9/12/16.
+ */
+
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+    public ItemViewHolder(View itemView) {
+        super(itemView);
+    }
+}
---
+++ app/src/main/java/swipe/example/com/swipeit/RecyclerListAdapter.java
@@ -0,0 +1,25 @@
+package swipe.example.com.swipeit;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by ngondo on 9/12/16.
+ */
+
+public class RecyclerListAdapter extends RecyclerView.Adapter<ItemViewHolder> {
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
