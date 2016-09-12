976d0d13235b957b364ad78ad0200de23285b60a
cngondo <devngondo@gmail.com>
2016-09-12 23:51:18 +0300

    initial commit

---
+++ app/src/main/java/swipe/example/com/swipeit/MainActivity.java
@@ -0,0 +1,13 @@
+package swipe.example.com.swipeit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
