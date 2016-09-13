04fa083c77cc669b1be92a0f63dfabc74a88aa46
cngondo <devngondo@gmail.com>
2016-09-13 13:26:51 +0300

    removed unused annotations

--- app/src/main/java/swipe/example/com/swipeit/MainFragment.java
+++ app/src/main/java/swipe/example/com/swipeit/MainFragment.java
@@ -2,7 +2,6 @@ package swipe.example.com.swipeit;
 
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -43,8 +42,6 @@ public class MainFragment extends Fragment {
         ItemTouchHelper.Callback callback = new SimpleItemTouchHelperAdapter(recyclerListAdapter);
         ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
         touchHelper.attachToRecyclerView(recyclerView);
-
-
 
     }
 }
